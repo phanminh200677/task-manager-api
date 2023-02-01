d08110afb90f9c912ef46bf316bca1c3c914e9a1
Phan Minh <pminh.vcbs@vcbs.com>
2023-02-01 17:14:00 +0700

    Modify user router

--- src/routers/user.js
+++ src/routers/user.js
@@ -23,7 +23,7 @@ router.post('/users/login', async(req, res) => {
         console.log(req.body)
         const user = await User.findByCredentials(req.body.email, req.body.password)                
         const token = await user.generateAuthToken()
-        res.send({ user, token })
+        res.status(200).send({ user, token })
     } catch (e) {        
         res.status(400).send()
     }
